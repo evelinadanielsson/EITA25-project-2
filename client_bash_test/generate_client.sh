df72e01f881e267691093987fd25751afeab6cdb
frallan97 <franssjos@gmail.com>
2021-02-17 09:50:24 +0100

    bash for generating clients

---
+++ client_bash_test/generate_client.sh
@@ -0,0 +1,32 @@
+#! /bin/sh
+
+mkdir $1
+cd $1
+
+password=evelina123
+country=SE
+state=Skane
+locality=Lund
+organization=Polisen
+organizationalunit=Polisen
+commonname=Evelina
+password=evelina123
+email=fake@email.com
+
+
+
+openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -passout pass:$password -passin pass:$password \
+        -subj "/C=$country/ST=$state/L=$locality/O=$organization/OU=$organizationalunit/CN=$commonname/emailAddress=$email" 
+
+keytool -import -file cert.pem -alias CA -keystore clienttruststore -storepass $password -noprompt
+
+keytool -genkey -alias key_pair_id  -keyalg RSA -validity 365 -keystore clientkeystore -storetype JKS -storepass $password -keypass $password \
+        -dname "CN=$commonname, OU=$organizationalunit, O=$organization, C=$country, ST=$state, L=$locality"
+
+keytool -certreq -alias key_pair_id  -file csr -keystore clientkeystore -storepass $password -noprompt
+
+openssl x509 -req -in csr -CA cert.pem -CAkey key.pem -CAcreateserial -out certificate_chain.pem -passin pass:$password
+
+keytool -importcert -alias CA -file cert.pem -keystore clientkeystore -storepass $password -noprompt
+
+keytool -importcert -alias key_pair_id -file certificate_chain.pem -keystore clientkeystore -storepass $password -noprompt
