78974c3afa612a3ddff696deaef58c4af2fe7c72
frallan97 <franssjos@gmail.com>
2021-02-19 14:03:30 +0100

    added clients and the bash scripts now work

--- server_clients_authority/Server_client_setup.sh
+++ server_clients_authority/Server_client_setup.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 gnome-terminal -e 'sh -c "cd server; java server 9876"' --geometry 100x200+0+0
-gnome-terminal -e 'sh -c "cd all_clients/emma_client_d;    java client localhost 9876"' --geometry 100x10-100+0
-# gnome-terminal -e 'sh -c "cd all_clients/frans_client_d;   java client localhost 9876"' --geometry 100x10-100+250
-# gnome-terminal -e 'sh -c "cd all_clients/evelina_client_p; java client localhost 9876"' --geometry 100x10-100+500
-# gnome-terminal -e 'sh -c "cd all_clients/joel_client_p;    java client localhost 9876"' --geometry 100x10-100+750
+gnome-terminal -e 'sh -c "cd emma_client;    java client localhost 9876"' --geometry 100x10-100+0
+gnome-terminal -e 'sh -c "cd frans_client;   java client localhost 9876"' --geometry 100x10-100+250
+gnome-terminal -e 'sh -c "cd evelina_client; java client localhost 9876"' --geometry 100x10-100+500
+gnome-terminal -e 'sh -c "cd joel_client;    java client localhost 9876"' --geometry 100x10-100+750
--- server_clients_authority/generate_client.sh
+++ server_clients_authority/generate_client.sh
@@ -4,31 +4,27 @@ mkdir $1
 cd $1
 
 password=password
-country=SE
-state=Skane
-locality=Lund
-organization=None
-organizationalunit=None
 commonname=CA
-email=fake@email.com
+cert_location=../authority/cert.pem
+key_location=../authority/key.pem
 
 
 
-openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -passout pass:$password -passin pass:$password \
-        -subj "CN=CA" 
+# openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365 -passout pass:$password -passin pass:$password \
+#         -subj "CN=CA" 
 
-#  keytool -import -file /authority/cert.pem -alias CA_$commonname -keystore clienttruststore -storepass $password -noprompt
+keytool -import -file $cert_location -alias CA -keystore clienttruststore -storepass $password -noprompt
 
-# keytool -genkey -alias key_pair_id_$commonname -keyalg RSA -validity 365 -keystore clientkeystore -storetype JKS -storepass $password -keypass $password \
-#         -dname "CN=$commonname, OU=$organizationalunit, O=$organization, C=$country, ST=$state, L=$locality"
+keytool -genkey -alias key_pair_id -keyalg RSA -validity 365 -keystore clientkeystore -storetype JKS -storepass $password -keypass $password \
+                -dname "CN=$commonname"
 
-# keytool -certreq -alias key_pair_id_$commonname -file csr -keystore clientkeystore -storepass $password -noprompt
+keytool -certreq -alias key_pair_id -file csr -keystore clientkeystore -storepass $password -noprompt
 
-# openssl x509 -req -in csr -CA cert.pem -CAkey key.pem -CAcreateserial -out certificate_chain.pem -passin pass:$password
+openssl x509 -req -in csr -CA $cert_location -CAkey $key_location -CAcreateserial -out certificate_chain.pem -passin pass:$password
 
-# keytool -importcert -alias CA_$commonname -file cert.pem -keystore clientkeystore -storepass $password -noprompt
+keytool -importcert -alias CA -file $cert_location -keystore clientkeystore -storepass $password -noprompt
 
-# keytool -importcert -alias key_pair_id_$commonname -file certificate_chain.pem -keystore clientkeystore -storepass $password -noprompt
+keytool -importcert -alias key_pair_id -file certificate_chain.pem -keystore clientkeystore -storepass $password -noprompt
 
-# cp ../../original_client.java client.java
-# javac client.java
+cp ../../original_client.java client.java
+javac client.java
