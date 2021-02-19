eca6d5ea8fb88d789f0f6f3fee9194a7fda7534d
frallan97 <franssjos@gmail.com>
2021-02-19 14:38:57 +0100

    changed so all clients have special common names

--- server_clients_authority/Server_client_setup.sh
+++ server_clients_authority/Server_client_setup.sh
@@ -4,3 +4,8 @@ gnome-terminal -e 'sh -c "cd emma_client;    java client localhost 9876"' --geom
 gnome-terminal -e 'sh -c "cd frans_client;   java client localhost 9876"' --geometry 100x10-100+250
 gnome-terminal -e 'sh -c "cd evelina_client; java client localhost 9876"' --geometry 100x10-100+500
 gnome-terminal -e 'sh -c "cd joel_client;    java client localhost 9876"' --geometry 100x10-100+750
+
+gnome-terminal -e 'sh -c "cd joel_client;    java client localhost 9876"' --geometry 100x10-100+750
+gnome-terminal -e 'sh -c "cd joel_client;    java client localhost 9876"' --geometry 100x10-100+750
+gnome-terminal -e 'sh -c "cd joel_client;    java client localhost 9876"' --geometry 100x10-100+750
+gnome-terminal -e 'sh -c "cd joel_client;    java client localhost 9876"' --geometry 100x10-100+750
